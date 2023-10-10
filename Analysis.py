c6e50b933ca498b02eae42053f870f08bd0dff13
PanagiotisKoulisoglou <panagiotiskoulisoglou@gmail.com>
2023-10-10 15:02:06 +0300

    Initial commit: Added Python code for Balance Sheet Analysis

---
+++ Analysis.py
@@ -0,0 +1,27 @@
+import requests
+import matplotlib.pyplot as plt
+
+api_key = '9e19211b3ea0d2f24da279632b57a423'
+
+company = "AAPL"
+years = 2
+
+balance_sheet = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company}?limit={years}&apikey={api_key}')
+balance_sheet_json = balance_sheet.json()
+
+totral_current_assets = balance_sheet_json[0]['totalCurrentAssets']
+print(f"Total Current Assets O  f {company}: {totral_current_assets:,}")
+
+totral_current_liabilities = balance_sheet_json[0]['totalCurrentLiabilities'] 
+print(f"Total Current Liabilities Of {company}: {totral_current_liabilities:,}")
+
+total_debt = balance_sheet_json[0]['totalDebt']
+cash_end_equivalents = balance_sheet_json[0]['cashAndCashEquivalents']
+cash_debt_difference = cash_end_equivalents - total_debt
+print(f"Cash Debt Difference Of {company}: {cash_debt_difference:,}")
+
+goodwill_and_intangibles = balance_sheet_json[0]['goodwillAndIntangibleAssets']
+total_assets = balance_sheet_json[0]['totalAssets']
+pct_intangibles = goodwill_and_intangibles / total_assets * 100 
+
+print(f"Percentage of Intangibles Of {company}: {pct_intangibles:,.2f}%")
---
+++ Analysis2.py
@@ -0,0 +1,25 @@
+import requests
+import matplotlib.pyplot as plt
+
+api_key = '9e19211b3ea0d2f24da279632b57a423'
+
+company = "AAPL"
+years = 4
+
+balance_sheet = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company}?period=quarterly&limit={years}&apikey={api_key}')
+balance_sheet_json = balance_sheet.json()
+
+assts_q1 = balance_sheet[4]['totalAssets']
+assts_q2 = balance_sheet[3]['totalAssets']
+assts_q3 = balance_sheet[2]['totalAssets']
+assts_q4 = balance_sheet[1]['totalAssets']
+
+assets_data = [assts_q1, assts_q2, assts_q3, assts_q4]
+assets_data = [x/100000000 for x in assets_data]
+
+plt.bar([1,2,3,4], assets_data)
+plt.title(f"Quarterly Assets of {company}")
+plt.xlabel("Quarter")
+plt.ylabel("Total Assets (in billions)")
+plt.xticks([1,2,3,4], ["Q1", "Q2", "Q3", "Q4"])
+plt.show()
